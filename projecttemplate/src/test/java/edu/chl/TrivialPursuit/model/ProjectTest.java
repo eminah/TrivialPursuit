e7620f6b42cc7f3a7c0d615d2ac754727986d699
Heléne Jarl <helenejarl@dhcp-187192.eduroam.chalmers.se>
2015-03-30 15:54:49 +0200

    maven

---
+++ projecttemplate/src/test/java/edu/chl/TrivialPursuit/model/ProjectTest.java
@@ -0,0 +1,21 @@
+package edu.chl.TrivialPursuit.model;
+
+import edu.chl.TrivialPursuit.model.Project;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProjectTest {
+	private static final int NUM_INCREMENTATIONS = 128;
+
+	@Test
+	public void testIncrementResult() {
+		final Project project = new Project();
+
+		for (int i = 0; i < NUM_INCREMENTATIONS; i++) {
+			project.incrementPresses();
+		}
+
+		Assert.assertEquals(NUM_INCREMENTATIONS, project.getPresses());
+	}
+}
