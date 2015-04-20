078afce6c6f70167556da1e490cd46e140ef1edb
inaatraan <inaatraan@gmail.com>
2015-04-20 10:48:13 +0200

    domain model

--- projecttemplate/src/main/java/edu/chl/TrivialPursuit/Main.java
+++ projecttemplate/src/main/java/edu/chl/TrivialPursuit/Main.java
@@ -16,11 +16,11 @@ public final class Main {
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
-                    final Project project = new Project();
-                    final ProjectView projectView = new ProjectView(project);
+            final Project project = new Project();
+            final ProjectView projectView = new ProjectView(project);
                     
-                    ProjectController.create(project, projectView);
-                    projectView.setVisible(true);
-                });
+            ProjectController.create(project, projectView);
+            projectView.setVisible(true);
+        });
 	}
 }
---
+++ projecttemplate/src/main/java/edu/chl/TrivialPursuit/view/StartView.java
@@ -0,0 +1,7 @@
+package edu.chl.trivialPursuit.view;
+
+/**
+ * Created by inatran on 15-04-20.
+ */
+public class StartView {
+}
