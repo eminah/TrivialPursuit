cb6e4d0c39fdbcb92968615987ef762a13a7bc7a
Rasti Tengman <rasti@student.chalmers.se>
2015-05-04 16:42:57 +0200

    start guit och css fil

--- projecttemplate/src/main/java/edu/chl/TrivialPursuit/controller/StartController.java
+++ projecttemplate/src/main/java/edu/chl/TrivialPursuit/controller/StartController.java
@@ -1,7 +1,17 @@
 package edu.chl.trivialpursuit.controller;
 
+import edu.chl.trivialpursuit.view.StartView;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
 /**
  * Created by inatran on 15-04-28.
  */
-public class StartController {
+public class StartController implements EventHandler<ActionEvent> {
+
+
+    @Override
+    public void handle(ActionEvent event) {
+
+    }
 }
