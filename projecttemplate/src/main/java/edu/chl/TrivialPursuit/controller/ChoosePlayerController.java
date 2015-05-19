f380384f57605f9b8f95382fb6fd8b063ebc29f6
inaatraan <inaatraan@gmail.com>
2015-05-19 12:38:35 +0200

    ChoosePlayerView

--- projecttemplate/src/main/java/edu/chl/TrivialPursuit/controller/ChoosePlayerController.java
+++ projecttemplate/src/main/java/edu/chl/TrivialPursuit/controller/ChoosePlayerController.java
@@ -1,9 +1,18 @@
 package edu.chl.trivialpursuit.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 /**
  * Created by Rasti on 2015-05-05.
  */
 public class ChoosePlayerController {
 
+    @FXML
+    private ComboBox<String> numPlayers;
 
+    @FXML
+    private void onButtonPressed() {
+        System.out.println("Hej");
+    }
 }
