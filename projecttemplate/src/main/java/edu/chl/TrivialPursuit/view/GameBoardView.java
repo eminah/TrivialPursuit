cbb48021bc01ec9410fb22760ff55bdda120a081
Helene Jarl <jhelene@student.chalmers.se>
2015-05-20 19:39:08 +0200

    Added pictures for the collection

--- projecttemplate/src/main/java/edu/chl/TrivialPursuit/view/GameBoardView.java
+++ projecttemplate/src/main/java/edu/chl/TrivialPursuit/view/GameBoardView.java
@@ -1,16 +1,18 @@
 package edu.chl.trivialpursuit.view;
-
-        import com.airhacks.afterburner.injection.Injector;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-
-        import java.io.IOException;
+import com.airhacks.afterburner.injection.Injector;
+import java.io.IOException;
 
 /**
  * Created by Rasti on 2015-05-04.
  */
 public class GameBoardView extends BaseView{
 
+    private GameBoardView() {super();}
+
+    public static GameBoardView create() throws  IOException{
+        final GameBoardView gameBoardView = new GameBoardView();
+        Injector.injectMembers(StartView.class, gameBoardView);
+        return gameBoardView;
+    }
 
 }
