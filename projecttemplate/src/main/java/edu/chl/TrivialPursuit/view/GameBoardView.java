470ba58069e126456f124d4b0f937fb28e73733b
Helene Jarl <jhelene@student.chalmers.se>
2015-05-20 18:27:56 +0200

    started on gameBoardView on wrong branch

--- projecttemplate/src/main/java/edu/chl/TrivialPursuit/view/GameBoardView.java
+++ projecttemplate/src/main/java/edu/chl/TrivialPursuit/view/GameBoardView.java
@@ -1,13 +1,16 @@
 package edu.chl.trivialpursuit.view;
 
+        import com.airhacks.afterburner.injection.Injector;
         import javafx.fxml.FXMLLoader;
         import javafx.scene.Parent;
         import javafx.scene.Scene;
 
+        import java.io.IOException;
+
 /**
  * Created by Rasti on 2015-05-04.
  */
-public class GameBoardView{
+public class GameBoardView extends BaseView{
 
 
 }
