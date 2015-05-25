3c6f7f5bb0ffe305d8ddbcb1119a489707679049
eminah <eminah@student.chalmers.se>
2015-05-25 15:01:19 +0200

    deleted class and implemented methods

--- projecttemplate/src/main/java/edu/chl/TrivialPursuit/controller/SettingsController.java
+++ projecttemplate/src/main/java/edu/chl/TrivialPursuit/controller/SettingsController.java
@@ -1,9 +1,27 @@
 package edu.chl.trivialpursuit.controller;
 
+import edu.chl.trivialpursuit.view.GameBoardView;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
 /**
  * Created by eminahromic on 2015-05-21.
  */
 public class SettingsController {
 
+
+
+    public void setMusic(){
+
+    }
+
+    public void getMusicVolume(){
+        
+    }
 
 }
