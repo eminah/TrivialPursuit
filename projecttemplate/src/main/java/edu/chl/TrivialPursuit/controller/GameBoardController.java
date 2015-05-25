f508ce17c83c5ea5130da328f31c704a88f261a7
Rasti Tengman <rasti@student.chalmers.se>
2015-05-25 02:22:39 +0200

    dont know

--- projecttemplate/src/main/java/edu/chl/TrivialPursuit/controller/GameBoardController.java
+++ projecttemplate/src/main/java/edu/chl/TrivialPursuit/controller/GameBoardController.java
@@ -81,8 +81,7 @@ public class GameBoardController implements Initializable {
     @FXML void moveLeft() throws IOException{
         movePlayerLeft(1, dice.getTotalDiceValue());
         drawBoard();
-        DiceView dice = DiceView.create();
-        dice.show();
+       
     }
 
     public void drawBoard(){
