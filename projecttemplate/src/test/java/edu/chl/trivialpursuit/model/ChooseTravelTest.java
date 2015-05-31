502d7ccb9dc0338d86e2eaea1411af366270391d
Helene Jarl <jhelene@student.chalmers.se>
2015-05-31 18:11:42 +0200

    test for chooseTravel

---
+++ projecttemplate/src/test/java/edu/chl/trivialpursuit/model/ChooseTravelTest.java
@@ -0,0 +1,17 @@
+package edu.chl.trivialpursuit.model;
+import junit.framework.TestCase;
+import org.junit.Test;
+/**
+ * Created by Heléne Jarl on 2015-05-31.
+ */
+public class ChooseTravelTest extends TestCase {
+    ChooseTravel startPlace = new ChooseTravel();
+    @Test
+    public void testSetStartPlace() throws Exception {
+        assertTrue(startPlace.continentToStartArray.isEmpty());
+        startPlace.setStartPlace("Africa", 1);
+        assertTrue(!startPlace.continentToStartArray.isEmpty());
+        assertEquals("Africa",startPlace.continentToStartArray.get(0));
+
+    }
+}
